b8726c1f216b3e56150b64fc5b64376932f6d1c3
celian <celian.riboulet@gmail.com>
2021-02-05 19:34:00 +0100

    ESLint fix

--- src/index.js
+++ src/index.js
@@ -9,48 +9,43 @@ const modulesTab = require("./modulesTab");
 //     description: 'ping pong!'
 // }})
 
-botClient.on('ready', () => {
-	console.log("Started !")
+botClient.on("ready", () => {
+	console.log("Started !");
 
-    botClient.api.applications(botClient.user.id).commands.post({
-        data: {
-            name: "agenda",
-            description: "Permet d'ajouter un devoir dans l'agenda Discord et Hudbay"
-        }
-    });
+	botClient.api.applications(botClient.user.id).commands.post({
+		data: {
+			name: "agenda",
+			description: "Permet d'ajouter un devoir dans l'agenda Discord et Hudbay"
+		}
+	});
 
 
-    botClient.ws.on('INTERACTION_CREATE', async interaction => {
-        const command = interaction.data.name.toLowerCase();
-        const args = interaction.data.options;
+	botClient.ws.on("INTERACTION_CREATE", async interaction => {
+		const command = interaction.data.name.toLowerCase();
 
-        if (command === 'agenda'){ 
+		if (command === "agenda"){ 
 			botClient.users.fetch(interaction.member.user.id)
-			.then(async (user) => {
-				console.log(user);
-				user.send(await testCanvas(["UE 1-1", "UE 1-2"]))
-			})
-        }
-    });
+				.then(async (user) => {
+					user.send(await getMatieresEmbed(["UE 1-1", "UE 1-2"]));
+				});
+		}
+	});
 });
 
-// botClient.on("message", msg => {
+botClient.on("message", msg => {
 
-// 	//On regarde si le message commence bien par le prefix (!)
-// 	if (!msg.content.startsWith(discordConfig.prefix))//Si le message ne commence pas par le prefix du config.json
-// 		return;
+	//On regarde si le message commence bien par le prefix (!)
+	if (!msg.content.startsWith(discordConfig.prefix))//Si le message ne commence pas par le prefix du config.json
+		return;
 
-// 	switch (msg.content.substr(1).split(" ")[0]) {//Switch sur le premier mot du msg sans le prefix Ex: "!agenda dejfez" donne "agenda"
-// 		case "test":
-// 			break;
-// 	}
-// });
+	switch (msg.content.substr(1).split(" ")[0]) {//Switch sur le premier mot du msg sans le prefix Ex: "!agenda dejfez" donne "agenda"
+		case "agenda-help":
+			break;
+	}
+});
 
-
-const testCanvas = async (ue) => {
-
+const getMatieresEmbed = async (ue) => {
 	const attachment = await modulesTab.getTabImageAttachment(ue);
-
 	const embed = new Discord.MessageEmbed()
 		.attachFiles(attachment)
 		.setColor("#afdab9")
@@ -58,8 +53,7 @@ const testCanvas = async (ue) => {
 		.setFooter("Repondez avec le numéro correspondant")
 		.setAuthor("Cliquez sur l'image pour zoomer !", "https://www.hubday.fr/favicon/apple-touch-icon.png");
 	return embed;
-}
-
+};
 
 
 botClient.login(discordConfig.token);
