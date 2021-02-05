9e05b1775a63fb679095dce4f356264bbad4f6f6
celian <celian.riboulet@gmail.com>
2021-02-05 19:34:20 +0100

    Modules tab add

---
+++ src/modulesTab.js
@@ -0,0 +1,109 @@
+const Discord = require("discord.js");
+
+const { CanvasTable} = require("canvas-table");
+const { createCanvas} = require("canvas");
+
+const modules = require("./modules");
+
+const getTabImageAttachment = async (ueList) => {
+	const mod = [];
+	ueList.forEach(element => {
+		getModulesByUE(element).forEach(matiere => {
+			mod.push(matiere);
+		});
+	});
+
+	const canvas = createCanvas(1920 / 3, 25 * mod.length + 50);
+
+	const data = getData(mod);
+
+	const ct = new CanvasTable(canvas, {
+		data,
+		columns,
+		options
+	});
+	await ct.generateTable();
+	await ct.renderToFile("test-table.png");
+
+	return new Discord.MessageAttachment(canvas.toBuffer(), "image.png");
+};
+
+/**
+ * Donne un ue
+ * Retourne tous les modules de cet ue
+ */
+const getModulesByUE = (ue) => {
+	let tab = [];
+	for (var mod in modules) {
+		if (modules[mod].ue == ue) {
+			tab.push(modules[mod]);
+		}
+	}
+	return tab;
+};
+
+
+const getData = (mods) => {
+	let data = [];
+	let i = 1;
+	mods.forEach(mod => {
+		const ligne = [i.toString(), mod.name, mod.ue];
+		data.push(ligne);
+		i++;
+	});
+	return data;
+};
+
+const columns = [
+	{
+		title: "Numero",
+		options: {
+			textAlign: "center",
+			fontSize: 20,
+			fontWeight: "bold",
+			fontFamily: "arial",
+			color: "white",
+			lineHeight: 3,
+		}
+	},
+	{
+		title: "Nom",
+		options: {
+			textAlign: "left",
+			fontSize: 20,
+			fontWeight: "bold",
+			fontFamily: "arial",
+			color: "white",
+			lineHeight: 3,
+		}
+	},
+	{
+		title: "UE",
+		options: {
+			textAlign: "center",
+			fontSize: 20,
+			fontWeight: "bold",
+			fontFamily: "arial",
+			color: "white",
+			lineHeight: 3,
+		}
+	}
+];
+
+const options = {
+	background: "#2c2f33",
+	borders: {
+		table: { color: "white", width: 3 }
+	},
+	fit: true,
+	options: {
+		textAlign: "center",
+	},
+	header: {
+		fontSize: 20,
+		fontFamily: "arial",
+		color: "#afdab9",
+	},
+};
+
+exports.getTabImageAttachment = getTabImageAttachment;
